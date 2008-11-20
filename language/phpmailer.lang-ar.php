88694da0048ed7017a35b3c9746e106f66b928ed
Marcus Bointon <marcus@synchromedia.co.uk>
2008-11-20 22:27:58 +0000

    Add smtp error string to all languages Remove stray comment chars in Arabic translation Correct UTF-8 encoding of Russian and Chinese translations Use single quotes in all array indices in language files Remove unnecessary string concatenations and blank lines Japanese and Polish are still broken

--- language/phpmailer.lang-ar.php
+++ language/phpmailer.lang-ar.php
@@ -2,24 +2,25 @@
 /**
  * PHPMailer language file.
  * Arabic Version, UTF-8
- * by : bahjat al mostafa <bahjat983@hotmail.com> */
+ * by : bahjat al mostafa <bahjat983@hotmail.com>
  */
 
 $PHPMAILER_LANG = array();
 
-$PHPMAILER_LANG["provide_address"] = 'You must provide at least one ' .
+$PHPMAILER_LANG['provide_address'] = 'You must provide at least one ' .
                                      'recipient email address.';
-$PHPMAILER_LANG["mailer_not_supported"] = ' mailer غير مدعوم.';
-$PHPMAILER_LANG["execute"] = 'لم أستطع تنفيذ : ';
-$PHPMAILER_LANG["instantiate"] = 'لم نستطع توفير خدمة البريد.';
-$PHPMAILER_LANG["authenticate"] = 'SMTP Error: لم نستطع تأكيد الهوية.';
-$PHPMAILER_LANG["from_failed"] = 'البريد التالي لم نستطع ارسال البريد له : ';
-$PHPMAILER_LANG["recipients_failed"] = 'SMTP Error: الأخطاء التالية ' .
+$PHPMAILER_LANG['mailer_not_supported'] = ' mailer غير مدعوم.';
+$PHPMAILER_LANG['execute'] = 'لم أستطع تنفيذ : ';
+$PHPMAILER_LANG['instantiate'] = 'لم نستطع توفير خدمة البريد.';
+$PHPMAILER_LANG['authenticate'] = 'SMTP Error: لم نستطع تأكيد الهوية.';
+$PHPMAILER_LANG['from_failed'] = 'البريد التالي لم نستطع ارسال البريد له : ';
+$PHPMAILER_LANG['recipients_failed'] = 'SMTP Error: الأخطاء التالية ' .
                                        'فشل في الارسال لكل من : ';
-$PHPMAILER_LANG["data_not_accepted"] = 'SMTP Error: لم يتم قبول المعلومات .';
-$PHPMAILER_LANG["connect_host"] = 'SMTP Error: لم نستطع الاتصال بمخدم SMTP.';
-$PHPMAILER_LANG["file_access"] = 'لم نستطع الوصول للملف: ';
-$PHPMAILER_LANG["file_open"] = 'File Error: لم نستطع فتح الملف: ';
-$PHPMAILER_LANG["encoding"] = 'ترميز غير معروف: ';
-$PHPMAILER_LANG["signing"] = 'خطأ في التوقيع: ';
+$PHPMAILER_LANG['data_not_accepted'] = 'SMTP Error: لم يتم قبول المعلومات .';
+$PHPMAILER_LANG['connect_host'] = 'SMTP Error: لم نستطع الاتصال بمخدم SMTP.';
+$PHPMAILER_LANG['file_access'] = 'لم نستطع الوصول للملف: ';
+$PHPMAILER_LANG['file_open'] = 'File Error: لم نستطع فتح الملف: ';
+$PHPMAILER_LANG['encoding'] = 'ترميز غير معروف: ';
+$PHPMAILER_LANG['signing'] = 'خطأ في التوقيع: ';
+$PHPMAILER_LANG['smtp_error'] = 'SMTP server error: ';
 ?>
--- language/phpmailer.lang-nl.php
+++ language/phpmailer.lang-nl.php
@@ -6,20 +6,18 @@
 
 $PHPMAILER_LANG = array();
 
-$PHPMAILER_LANG["provide_address"]      = 'Er moet tenmiste één ' .
-                                          'ontvanger emailadres opgegeven worden.';
-$PHPMAILER_LANG["mailer_not_supported"] = ' mailer wordt niet ondersteund.';
-$PHPMAILER_LANG["execute"]              = 'Kon niet uitvoeren: ';
-$PHPMAILER_LANG["instantiate"]          = 'Kon mail functie niet initialiseren.';
-$PHPMAILER_LANG["authenticate"]         = 'SMTP Fout: authenticatie mislukt.';
-$PHPMAILER_LANG["from_failed"]          = 'De volgende afzender adressen zijn mislukt: ';
-$PHPMAILER_LANG["recipients_failed"]    = 'SMTP Fout: De volgende ' .
-                                          'ontvangers zijn mislukt: ';
-$PHPMAILER_LANG["data_not_accepted"]    = 'SMTP Fout: Data niet geaccepteerd.';
-$PHPMAILER_LANG["connect_host"]         = 'SMTP Fout: Kon niet verbinden met SMTP host.';
-$PHPMAILER_LANG["file_access"]          = 'Kreeg geen toegang tot bestand: ';
-$PHPMAILER_LANG["file_open"]            = 'Bestandsfout: Kon bestand niet openen: ';
-$PHPMAILER_LANG["encoding"]             = 'Onbekende codering: ';
-$PHPMAILER_LANG["signing"]              = 'Signing Error: ';
-
+$PHPMAILER_LANG['provide_address']      = 'Er moet tenmiste één ontvanger emailadres opgegeven worden.';
+$PHPMAILER_LANG['mailer_not_supported'] = ' mailer wordt niet ondersteund.';
+$PHPMAILER_LANG['execute']              = 'Kon niet uitvoeren: ';
+$PHPMAILER_LANG['instantiate']          = 'Kon mail functie niet initialiseren.';
+$PHPMAILER_LANG['authenticate']         = 'SMTP Fout: authenticatie mislukt.';
+$PHPMAILER_LANG['from_failed']          = 'De volgende afzender adressen zijn mislukt: ';
+$PHPMAILER_LANG['recipients_failed']    = 'SMTP Fout: De volgende ontvangers zijn mislukt: ';
+$PHPMAILER_LANG['data_not_accepted']    = 'SMTP Fout: Data niet geaccepteerd.';
+$PHPMAILER_LANG['connect_host']         = 'SMTP Fout: Kon niet verbinden met SMTP host.';
+$PHPMAILER_LANG['file_access']          = 'Kreeg geen toegang tot bestand: ';
+$PHPMAILER_LANG['file_open']            = 'Bestandsfout: Kon bestand niet openen: ';
+$PHPMAILER_LANG['encoding']             = 'Onbekende codering: ';
+$PHPMAILER_LANG['signing']              = 'Signing Error: ';
+$PHPMAILER_LANG['smtp_error']           = 'SMTP server error: ';
 ?>
