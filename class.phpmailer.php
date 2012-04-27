fb70deb117abcfe78b3471568916b88bfd87e275
Synchro <marcus@synchromedia.co.uk>
2012-04-27 21:54:57 +0200

    Reapply charset quiting fix

--- class.phpmailer.php
+++ class.phpmailer.php
@@ -1259,7 +1259,7 @@ class PHPMailer {
     switch($this->message_type) {
       case 'plain':
         $result .= $this->HeaderLine('Content-Transfer-Encoding', $this->Encoding);
-        $result .= $this->TextLine('Content-Type: '.$this->ContentType.'; charset="'.$this->CharSet.'"');
+        $result .= $this->TextLine('Content-Type: '.$this->ContentType.'; charset='.$this->CharSet);
         break;
       case 'inline':
         $result .= $this->HeaderLine('Content-Type', 'multipart/related;');
@@ -1444,7 +1444,7 @@ class PHPMailer {
       $encoding = $this->Encoding;
     }
     $result .= $this->TextLine('--' . $boundary);
-    $result .= sprintf("Content-Type: %s; charset=\"%s\"", $contentType, $charSet);
+    $result .= sprintf("Content-Type: %s; charset=%s", $contentType, $charSet);
     $result .= $this->LE;
     $result .= $this->HeaderLine('Content-Transfer-Encoding', $encoding);
     $result .= $this->LE;
