07f7a1bdd51ce179712a00da016ad1f1823cebe1
Synchro <marcus@synchromedia.co.uk>
2013-02-02 18:41:54 +0100

    Add fake sendmail script, adjust travis config to use it

---
+++ test/fakesendmail.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#Fake sendmail script, adapted from:
+#https://github.com/mrded/MNPP/blob/ee64fb2a88efc70ba523b78e9ce61f9f1ed3b4a9/init/fake-sendmail.sh
+numPath="/tmp/fakemail"
+
+mkdir -p $numPath
+
+if [ ! -f $numPath/num ]; then
+echo "0" > $numPath/num
+fi
+num=`cat $numPath/num`
+num=$(($num + 1))
+echo $num > $numPath/num
+
+name="$numPath/letter_$num.txt"
+while read line
+do
+echo $line >> $name
+done
+chmod 777 $name
+/usr/bin/true
