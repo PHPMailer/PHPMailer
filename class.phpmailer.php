fd1b6e3b7f27b77d9106c2f69ded7f040c05bfd4
Inventorem <me@invntrm.ru>
2014-05-28 03:50:56 +0400

    Fix http:// ...

--- class.phpmailer.php
+++ class.phpmailer.php
@@ -1125,7 +1125,7 @@ class PHPMailer
      * Send mail using the PHP mail() function.
      * @param string $header The message headers
      * @param string $body The message body
-     * @link http:// www.php.net/manual/en/book.mail.php
+     * @link http://www.php.net/manual/en/book.mail.php
      * @throws phpmailerException
      * @access protected
      * @return bool
