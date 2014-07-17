d76eef679deafc22f6bc86f64c6624ae4fad9b77
JFox <jfox@jfox.sk>
2014-07-17 15:55:49 +0200

    Set default charset to UTF-8
    Set default charset to UTF-8.
    When charset=iso-8859-1 does not work for example Slovak diacritics.

--- class.phpmailer.php
+++ class.phpmailer.php
@@ -44,7 +44,7 @@ class PHPMailer
      * The character set of the message.
      * @type string
      */
-    public $CharSet = 'iso-8859-1';
+    public $CharSet = 'utf-8';
 
     /**
      * The MIME Content-type of the message.
