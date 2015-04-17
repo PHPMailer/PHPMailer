3c29d754fdc957e6ddf7bf7e4f8187ddf47468c4
Synchro <marcus@synchromedia.co.uk>
2015-04-17 14:48:00 +0200

    Improve connection error reporting

--- class.phpmailer.php
+++ class.phpmailer.php
@@ -1365,6 +1365,7 @@ class PHPMailer
                     return true;
                 } catch (phpmailerException $exc) {
                     $lastexception = $exc;
+                    $this->edebug($exc->getMessage());
                     // We must have connected, but then failed TLS or Auth, so close connection nicely
                     $this->smtp->quit();
                 }
