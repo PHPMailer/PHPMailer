a0407c6630818a57323cec96413d33bcafa8685a
Synchro <marcus@synchromedia.co.uk>
2015-11-10 19:09:08 +0100

    Travis & PHPUnit tweaks

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -2,6 +2,8 @@
 /**
  * PHPUnit bootstrap file
  */
+
+ini_set('sendmail_path', '/usr/sbin/sendmail -t -i ');
 require_once '../vendor/autoload.php';
 spl_autoload_register(
     function ($class) {
