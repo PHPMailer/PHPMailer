5b49262b3c0bed655182f26670366abfa9b39247
Andrew Nagy <tm1000@users.noreply.github.com>
2015-11-09 16:37:27 -0800

    Fix unit tests to load the correct autoloader

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'vendor/autoload.php';
+require_once dirname(__DIR__).'/PHPMailerAutoload.php';
 spl_autoload_register(function ($class) {
     require_once strtr($class, '\\_', '//').'.php';
 });
