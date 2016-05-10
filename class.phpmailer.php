d0186171b28af4f06ac2ad8a84a8f3d6cbc3ba6c
Marcus Bointon <marcus@synchromedia.co.uk>
2016-05-10 20:39:36 +0200

    Bump version to 5.2.15

--- class.phpmailer.php
+++ class.phpmailer.php
@@ -31,7 +31,7 @@ class PHPMailer
      * The PHPMailer Version number.
      * @var string
      */
-    public $Version = '5.2.14';
+    public $Version = '5.2.15';
 
     /**
      * Email priority.
