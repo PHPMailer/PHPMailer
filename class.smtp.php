a4de1ca5efeebf155a2a0a17840c19ac53555dee
Marcus Bointon <marcus@synchromedia.co.uk>
2016-10-17 18:21:50 +0200

    Pass level of current debug message rather than output setting into display function

--- class.smtp.php
+++ class.smtp.php
@@ -206,7 +206,7 @@ class SMTP
         }
         //Avoid clash with built-in function names
         if (!in_array($this->Debugoutput, array('error_log', 'html', 'echo')) and is_callable($this->Debugoutput)) {
-            call_user_func($this->Debugoutput, $str, $this->do_debug);
+            call_user_func($this->Debugoutput, $str, $level);
             return;
         }
         switch ($this->Debugoutput) {
