d1a7de4e94a7c46aba9e00440ad291681feee5f6
Marcus Bointon <marcus@synchromedia.co.uk>
2017-01-06 22:27:49 +0100

    Make test bootstrap a bit more flexible

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,5 +1,9 @@
 <?php
-require_once 'vendor/autoload.php';
+if (file_exists('vendor/autoload.php')) {
+    require_once 'vendor/autoload.php';
+} else {
+    require_once '../vendor/autoload.php';
+}
 spl_autoload_register(function ($class) {
     require_once strtr($class, '\\_', '//').'.php';
 });
