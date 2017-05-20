ad33c20d0637f8c43ab2578ee332645dd29a524f
Marcus Bointon <marcus@synchromedia.co.uk>
2017-05-20 23:33:59 +0200

    Include timestamps in HTML-format debug output

--- class.smtp.php
+++ class.smtp.php
@@ -227,7 +227,7 @@ class SMTP
                 break;
             case 'html':
                 //Cleans up output a bit for a better looking, HTML-safe output
-                echo htmlentities(
+                echo gmdate('Y-m-d H:i:s') . ' ' . htmlentities(
                     preg_replace('/[\r\n]+/', '', $str),
                     ENT_QUOTES,
                     'UTF-8'
