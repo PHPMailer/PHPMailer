819f540c771da36dd50a7763dd720b2f19740d5d
Marcus Bointon <marcus@synchromedia.co.uk>
2017-09-12 09:17:58 +0200

    Reset to SMTP mailer for line break tests

--- test/phpmailerTest.php
+++ test/phpmailerTest.php
@@ -1936,6 +1936,8 @@ EOT;
      */
     public function testLineBreaks()
     {
+        //May have been altered by earlier tests, can interfere with line break format
+        $this->Mail->isSMTP();
         $unixsrc = "hello\nWorld\nAgain\n";
         $macsrc = "hello\rWorld\rAgain\r";
         $windowssrc = "hello\r\nWorld\r\nAgain\r\n";
@@ -1970,6 +1972,8 @@ EOT;
      */
     public function testLineLength()
     {
+        //May have been altered by earlier tests, can interfere with line break format
+        $this->Mail->isSMTP();
         $oklen = str_repeat(str_repeat('0', PHPMailer::MAX_LINE_LENGTH) . "\r\n", 2);
         $badlen = str_repeat(str_repeat('1', PHPMailer::MAX_LINE_LENGTH + 1) . "\r\n", 2);
         $this->assertTrue(PHPMailer::hasLineLongerThanMax($badlen), 'Long line not detected (only)');
