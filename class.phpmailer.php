dde1db116511aa4956389d75546c5be4c2beb2a6
Marcus Bointon <marcus@synchromedia.co.uk>
2018-11-15 23:32:31 +0100

    5.2.27

--- class.phpmailer.php
+++ class.phpmailer.php
@@ -31,7 +31,7 @@ class PHPMailer
      * The PHPMailer Version number.
      * @var string
      */
-    public $Version = '5.2.26';
+    public $Version = '5.2.27';
 
     /**
      * Email priority.
--- class.pop3.php
+++ class.pop3.php
@@ -34,7 +34,7 @@ class POP3
      * @var string
      * @access public
      */
-    public $Version = '5.2.26';
+    public $Version = '5.2.27';
 
     /**
      * Default POP3 port number.
