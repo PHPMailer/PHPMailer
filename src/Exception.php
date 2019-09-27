5c7af569b4f80578306958378a48359dcdebe9c6
Marcus Bointon <marcus@synchromedia.co.uk>
2019-09-27 19:04:00 +0200

    CS

--- src/Exception.php
+++ src/Exception.php
@@ -23,7 +23,7 @@ namespace PHPMailer\PHPMailer;
 /**
  * PHPMailer exception handler.
  *
- * @author  Marcus Bointon <phpmailer@synchromedia.co.uk>
+ * @author Marcus Bointon <phpmailer@synchromedia.co.uk>
  */
 class Exception extends \Exception
 {
