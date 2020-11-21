91a3798d70ae7962635161b6a2b96e4966f3023a
Om Tiwari <optiwari.india@gmail.com>
2020-11-22 05:08:40 +0530

    Sun Nov 22 05:08:40 IST 2020

--- language/phpmailer.lang-hi.php
+++ language/phpmailer.lang-hi.php
@@ -3,25 +3,25 @@
 /**
  * Hindi PHPMailer language file: refer to English translation for definitive list
  * @package PHPMailer
- * @author Yash Karanke <mr.karanke@gmail.com>
+ * @author Om Prakash Tiwari <optiwari.india@gmail.com>
  */
 
-$PHPMAILER_LANG['authenticate']         = 'SMTP त्रुटि: प्रामाणिकता की जांच नहीं हो सका। ';
-$PHPMAILER_LANG['connect_host']         = 'SMTP त्रुटि: SMTP सर्वर से कनेक्ट नहीं हो सका। ';
-$PHPMAILER_LANG['data_not_accepted']    = 'SMTP त्रुटि: डेटा स्वीकार नहीं किया जाता है। ';
-$PHPMAILER_LANG['empty_message']        = 'संदेश खाली है। ';
-$PHPMAILER_LANG['encoding']             = 'अज्ञात एन्कोडिंग प्रकार। ';
-$PHPMAILER_LANG['execute']              = 'आदेश को निष्पादित करने में विफल। ';
-$PHPMAILER_LANG['file_access']          = 'फ़ाइल उपलब्ध नहीं है। ';
-$PHPMAILER_LANG['file_open']            = 'फ़ाइल त्रुटि: फाइल को खोला नहीं जा सका। ';
-$PHPMAILER_LANG['from_failed']          = 'प्रेषक का पता गलत है। ';
-$PHPMAILER_LANG['instantiate']          = 'मेल फ़ंक्शन कॉल नहीं कर सकता है।';
-$PHPMAILER_LANG['invalid_address']      = 'पता गलत है। ';
-$PHPMAILER_LANG['mailer_not_supported'] = 'मेल सर्वर के साथ काम नहीं करता है। ';
-$PHPMAILER_LANG['provide_address']      = 'आपको कम से कम एक प्राप्तकर्ता का ई-मेल पता प्रदान करना होगा।';
-$PHPMAILER_LANG['recipients_failed']    = 'SMTP त्रुटि: निम्न प्राप्तकर्ताओं को पते भेजने में विफल। ';
+$PHPMAILER_LANG['authenticate']         = 'त्रुटि SMTP: विफल प्रमाणन';
+$PHPMAILER_LANG['connect_host']         = 'त्रुटि SMTP: अनुपलब्ध आतिथेय';
+$PHPMAILER_LANG['data_not_accepted']    = 'त्रुटि SMTP: अस्विकार्य';
+$PHPMAILER_LANG['empty_message']        = 'रिक्त संदेश';
+$PHPMAILER_LANG['encoding']             = 'अज्ञात एन्कोडिंग';
+$PHPMAILER_LANG['execute']              = 'विफल निष्पादन';
+$PHPMAILER_LANG['file_access']          = 'त्रुटि अनुमति: फाइल पर अनुमति उपलब्ध नहीं है ';
+$PHPMAILER_LANG['file_open']            = 'त्रुटि फाइल: अपठ्य फाइल ';
+$PHPMAILER_LANG['from_failed']          = 'अज्ञात प्रेषक ';
+$PHPMAILER_LANG['instantiate']          = 'अनुपलब्ध मेल ';
+$PHPMAILER_LANG['invalid_address']      = 'अज्ञात प्राप्तकर्ता ';
+$PHPMAILER_LANG['mailer_not_supported'] = 'असहायक मेल सर्वर';
+$PHPMAILER_LANG['provide_address']      = 'मेल का पता अनिवार्य है ';
+$PHPMAILER_LANG['recipients_failed']    = 'अज्ञात प्राप्तकर्ता ';
 $PHPMAILER_LANG['signing']              = 'साइनअप त्रुटि:। ';
-$PHPMAILER_LANG['smtp_connect_failed']  = 'SMTP का connect () फ़ंक्शन विफल हुआ। ';
-$PHPMAILER_LANG['smtp_error']           = 'SMTP सर्वर त्रुटि। ';
+$PHPMAILER_LANG['smtp_connect_failed']  = 'SMTP का connect() विफल रहा ';
+$PHPMAILER_LANG['smtp_error']           = 'त्रुटि SMTP सर्वर ';
 $PHPMAILER_LANG['variable_set']         = 'चर को बना या संशोधित नहीं किया जा सकता। ';
-$PHPMAILER_LANG['extension_missing']    = 'एक्सटेन्षन गायब है: ';
+$PHPMAILER_LANG['extension_missing']    = 'अनुपलब्ध एक्सटेनशन ';
