ad1851b6227095cbf13eb199990d9b2158a071a9
jrfnl <jrfnl@users.noreply.github.com>
2021-06-23 10:39:47 +0200

    ICalTest: reorganize to use data providers
    These three tests were 99% duplicate code. Using a data provider removes the duplication, while the actual tests being run are still 100% the same, including same test count and assertion count.

--- test/PHPMailer/ICalTest.php
+++ test/PHPMailer/ICalTest.php
@@ -23,8 +23,13 @@ final class ICalTest extends TestCase
 
     /**
      * Test ICal method
+     *
+     * @dataProvider dataICalMethod
+     *
+     * @param string $methodLine The Ical method line to use.
+     * @param string $expected   The expected content type header.
      */
-    public function testICalMethod()
+    public function testICalMethod($methodLine, $expected)
     {
         $this->Mail->Subject .= ': ICal method';
         $this->Mail->Body = '<h3>ICal method test.</h3>';
@@ -32,7 +37,7 @@ final class ICalTest extends TestCase
         $this->Mail->Ical = 'BEGIN:VCALENDAR'
             . "\r\nVERSION:2.0"
             . "\r\nPRODID:-//PHPMailer//PHPMailer Calendar Plugin 1.0//EN"
-            . "\r\nMETHOD:CANCEL"
+            . $methodLine
             . "\r\nCALSCALE:GREGORIAN"
             . "\r\nX-MICROSOFT-CALSCALE:GREGORIAN"
             . "\r\nBEGIN:VEVENT"
@@ -57,92 +62,34 @@ final class ICalTest extends TestCase
         $this->buildBody();
         $this->Mail->preSend();
         self::assertMatchesRegularExpression(
-            '/Content-Type: text\/calendar; method=CANCEL;/',
+            $expected,
             $this->Mail->getSentMIMEMessage(),
             'Wrong ICal method in Content-Type header'
         );
     }
 
     /**
-     * Test ICal missing method to use default (REQUEST)
+     * Data provider.
+     *
+     * @return array
      */
-    public function testICalInvalidMethod()
+    public function dataICalMethod()
     {
-        $this->Mail->Subject .= ': ICal method';
-        $this->Mail->Body = '<h3>ICal method test.</h3>';
-        $this->Mail->AltBody = 'ICal method test.';
-        $this->Mail->Ical = 'BEGIN:VCALENDAR'
-            . "\r\nVERSION:2.0"
-            . "\r\nPRODID:-//PHPMailer//PHPMailer Calendar Plugin 1.0//EN"
-            . "\r\nMETHOD:INVALID"
-            . "\r\nCALSCALE:GREGORIAN"
-            . "\r\nX-MICROSOFT-CALSCALE:GREGORIAN"
-            . "\r\nBEGIN:VEVENT"
-            . "\r\nUID:201909250755-42825@test"
-            . "\r\nDTSTART;20190930T080000Z"
-            . "\r\nSEQUENCE:2"
-            . "\r\nTRANSP:OPAQUE"
-            . "\r\nSTATUS:CONFIRMED"
-            . "\r\nDTEND:20190930T084500Z"
-            . "\r\nLOCATION:[London] London Eye"
-            . "\r\nSUMMARY:Test ICal method"
-            . "\r\nATTENDEE;CN=Attendee, Test;ROLE=OPT-PARTICIPANT;PARTSTAT=NEEDS-ACTION;RSVP="
-            . "\r\n TRUE:MAILTO:attendee-test@example.com"
-            . "\r\nCLASS:PUBLIC"
-            . "\r\nDESCRIPTION:Some plain text"
-            . "\r\nORGANIZER;CN=\"Example, Test\":MAILTO:test@example.com"
-            . "\r\nDTSTAMP:20190925T075546Z"
-            . "\r\nCREATED:20190925T075709Z"
-            . "\r\nLAST-MODIFIED:20190925T075546Z"
-            . "\r\nEND:VEVENT"
-            . "\r\nEND:VCALENDAR";
-        $this->buildBody();
-        $this->Mail->preSend();
-        self::assertMatchesRegularExpression(
-            '/Content-Type: text\/calendar; method=REQUEST;/',
-            $this->Mail->getSentMIMEMessage(),
-            'Wrong ICal method in Content-Type header'
-        );
-    }
-
-    /**
-     * Test ICal invalid method to use default (REQUEST)
-     */
-    public function testICalDefaultMethod()
-    {
-        $this->Mail->Subject .= ': ICal method';
-        $this->Mail->Body = '<h3>ICal method test.</h3>';
-        $this->Mail->AltBody = 'ICal method test.';
-        $this->Mail->Ical = 'BEGIN:VCALENDAR'
-            . "\r\nVERSION:2.0"
-            . "\r\nPRODID:-//PHPMailer//PHPMailer Calendar Plugin 1.0//EN"
-            . "\r\nCALSCALE:GREGORIAN"
-            . "\r\nX-MICROSOFT-CALSCALE:GREGORIAN"
-            . "\r\nBEGIN:VEVENT"
-            . "\r\nUID:201909250755-42825@test"
-            . "\r\nDTSTART;20190930T080000Z"
-            . "\r\nSEQUENCE:2"
-            . "\r\nTRANSP:OPAQUE"
-            . "\r\nSTATUS:CONFIRMED"
-            . "\r\nDTEND:20190930T084500Z"
-            . "\r\nLOCATION:[London] London Eye"
-            . "\r\nSUMMARY:Test ICal method"
-            . "\r\nATTENDEE;CN=Attendee, Test;ROLE=OPT-PARTICIPANT;PARTSTAT=NEEDS-ACTION;RSVP="
-            . "\r\n TRUE:MAILTO:attendee-test@example.com"
-            . "\r\nCLASS:PUBLIC"
-            . "\r\nDESCRIPTION:Some plain text"
-            . "\r\nORGANIZER;CN=\"Example, Test\":MAILTO:test@example.com"
-            . "\r\nDTSTAMP:20190925T075546Z"
-            . "\r\nCREATED:20190925T075709Z"
-            . "\r\nLAST-MODIFIED:20190925T075546Z"
-            . "\r\nEND:VEVENT"
-            . "\r\nEND:VCALENDAR";
-        $this->buildBody();
-        $this->Mail->preSend();
-        self::assertMatchesRegularExpression(
-            '/Content-Type: text\/calendar; method=REQUEST;/',
-            $this->Mail->getSentMIMEMessage(),
-            'Wrong ICal method in Content-Type header'
-        );
+        return [
+            'Valid method: cancel' => [
+                'methodLine' => "\r\nMETHOD:CANCEL",
+                'expected'   => '/Content-Type: text\/calendar; method=CANCEL;/',
+            ],
+            // Test ICal invalid method to use default (REQUEST).
+            'Invalid method' => [
+                'methodLine' => "\r\nMETHOD:INVALID",
+                'expected'   => '/Content-Type: text\/calendar; method=REQUEST;/',
+            ],
+            // Test ICal missing method to use default (REQUEST).
+            'Missing method' => [
+                'methodLine' => '',
+                'expected'   => '/Content-Type: text\/calendar; method=REQUEST;/',
+            ],
+        ];
     }
 }
