d2780d4125780c1700994e467333ece8614f0f38
Juliette <663378+jrfnl@users.noreply.github.com>
2021-06-24 13:20:27 +0200

    Tests: move tests related to DKIM to separate file (#2381)
    * Tests/reorganize: move DKIM related tests to own file
    
    * DKIMTest: stabilize removal of private key file
    
    When an assertion in a test fails, the rest of the code within the test method is no longer executed.
    This means that for a test which - like three out of the five DKIM test - creates a file, the test "clean up" via `unlink()` is also no longer executed.
    It also means that one test may - unintentionally - interfere with the results of another test due to the file still existing, while it is expected to have been removed.
    
    Aside from that, the `$privatekeyfile` variable used in the various test is the same everywhere, but not consistently used in the tests, as the `'dkim_private.pem'` is hard-coded in multiple places, which decreases maintainability.
    
    This commit fixes both these issues by:
    * Declaring a class constant with the target private key file name for use in the test method.
    * Implements use of this constant throughout the tests.
    * Removes the `unlink()` call from the individual tests, in favour of executing it via the test `tear_down()`, which should still be executed, even when a test has failed.
        Note: as the file also contains two tests which do not create the private key file, but for which the `tear_down()` would also be executed, the `unlink()` call is wrapped in a `file_exists()`.
    
    * DKIMTest: move `@group` tags
    
    ... to the class level and remove them from the individual test functions.
    
    * DKIMTest: add missing `@requires` tags
    
    Three out of the five tests actually require the `openssl` extension. Only one was so marked.
    
    * DKIMTest: various test tweaks
    
    Minor test tweaks:
    * Rename a test to a more specific name to allow for easier test filtering via PHPUnit.
    * Add `@covers` tags (Needs review!)
    * The `@see` tag is indented for code elements. For external links, the `@link` tag should be used.
    * Inline comment punctuation.
    * Minor code readability tweaks.
    
    * DKIMTest: add two additional tests
    
    ... to cover the "open SSL" not available case.
    
    Co-authored-by: jrfnl <jrfnl@users.noreply.github.com>

---
+++ test/PHPMailer/DKIMTest.php
@@ -0,0 +1,256 @@
+<?php
+
+/**
+ * PHPMailer - PHP email transport unit tests.
+ * PHP version 5.5.
+ *
+ * @author    Marcus Bointon <phpmailer@synchromedia.co.uk>
+ * @author    Andy Prevost
+ * @copyright 2012 - 2020 Marcus Bointon
+ * @copyright 2004 - 2009 Andy Prevost
+ * @license   http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License
+ */
+
+namespace PHPMailer\Test\PHPMailer;
+
+use Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\Test\TestCase;
+
+/**
+ * Test DKIM signing functionality.
+ *
+ * @group dkim
+ */
+final class DKIMTest extends TestCase
+{
+
+    const PRIVATE_KEY_FILE = 'dkim_private.pem';
+
+    /**
+     * Run after each test is completed.
+     */
+    protected function tear_down()
+    {
+        if (file_exists(self::PRIVATE_KEY_FILE)) {
+            unlink(self::PRIVATE_KEY_FILE);
+        }
+
+        parent::tear_down();
+    }
+
+    /**
+     * DKIM body canonicalization tests.
+     *
+     * @link https://tools.ietf.org/html/rfc6376#section-3.4.4
+     *
+     * @covers \PHPMailer\PHPMailer\PHPMailer::DKIM_BodyC
+     */
+    public function testDKIMBodyCanonicalization()
+    {
+        // Example from https://tools.ietf.org/html/rfc6376#section-3.4.5.
+        $prebody = " C \r\nD \t E\r\n\r\n\r\n";
+        $postbody = " C \r\nD \t E\r\n";
+
+        self::assertSame("\r\n", $this->Mail->DKIM_BodyC(''), 'DKIM empty body canonicalization incorrect');
+        self::assertSame(
+            'frcCV1k9oG9oKj3dpUqdJg1PxRT2RSN/XKdLCPjaYaY=',
+            base64_encode(hash('sha256', $this->Mail->DKIM_BodyC(''), true)),
+            'DKIM canonicalized empty body hash mismatch'
+        );
+        self::assertSame($postbody, $this->Mail->DKIM_BodyC($prebody), 'DKIM body canonicalization incorrect');
+    }
+
+    /**
+     * DKIM header canonicalization tests.
+     *
+     * @link https://tools.ietf.org/html/rfc6376#section-3.4.2
+     *
+     * @covers \PHPMailer\PHPMailer\PHPMailer::DKIM_HeaderC
+     */
+    public function testDKIMHeaderCanonicalization()
+    {
+        // Example from https://tools.ietf.org/html/rfc6376#section-3.4.5.
+        $preheaders = "A: X\r\nB : Y\t\r\n\tZ  \r\n";
+        $postheaders = "a:X\r\nb:Y Z\r\n";
+        self::assertSame(
+            $postheaders,
+            $this->Mail->DKIM_HeaderC($preheaders),
+            'DKIM header canonicalization incorrect'
+        );
+
+        // Check that long folded lines with runs of spaces are canonicalized properly.
+        $preheaders = 'Long-Header-1: <https://example.com/somescript.php?' .
+            "id=1234567890&name=Abcdefghijklmnopquestuvwxyz&hash=\r\n abc1234\r\n" .
+            "Long-Header-2: This  is  a  long  header  value  that  contains  runs  of  spaces and trailing    \r\n" .
+            ' and   is   folded   onto   2   lines';
+        $postheaders = 'long-header-1:<https://example.com/somescript.php?id=1234567890&' .
+            "name=Abcdefghijklmnopquestuvwxyz&hash= abc1234\r\nlong-header-2:This is a long" .
+            ' header value that contains runs of spaces and trailing and is folded onto 2 lines';
+        self::assertSame(
+            $postheaders,
+            $this->Mail->DKIM_HeaderC($preheaders),
+            'DKIM header canonicalization of long lines incorrect'
+        );
+    }
+
+    /**
+     * DKIM copied header fields tests.
+     *
+     * @link https://tools.ietf.org/html/rfc6376#section-3.5
+     *
+     * @requires extension openssl
+     *
+     * @covers \PHPMailer\PHPMailer\PHPMailer::DKIM_Add
+     * @covers \PHPMailer\PHPMailer\PHPMailer::DKIM_Sign
+     * @covers \PHPMailer\PHPMailer\PHPMailer::DKIM_QP
+     */
+    public function testDKIMOptionalHeaderFieldsCopy()
+    {
+        $pk = openssl_pkey_new(
+            [
+                'private_key_bits' => 2048,
+                'private_key_type' => OPENSSL_KEYTYPE_RSA,
+            ]
+        );
+        openssl_pkey_export_to_file($pk, self::PRIVATE_KEY_FILE);
+        $this->Mail->DKIM_private = self::PRIVATE_KEY_FILE;
+
+        // Example from https://tools.ietf.org/html/rfc6376#section-3.5.
+        $from = 'from@example.com';
+        $to = 'to@example.com';
+        $date = 'date';
+        $subject = 'example';
+
+        $headerLines = "From:$from\r\nTo:$to\r\nDate:$date\r\n";
+        $copyHeaderFields = " z=From:$from\r\n |To:$to\r\n |Date:$date\r\n |Subject:$subject;\r\n";
+
+        $this->Mail->DKIM_copyHeaderFields = true;
+        self::assertStringContainsString(
+            $copyHeaderFields,
+            $this->Mail->DKIM_Add($headerLines, $subject, ''),
+            'DKIM header with copied header fields incorrect'
+        );
+
+        $this->Mail->DKIM_copyHeaderFields = false;
+        self::assertStringNotContainsString(
+            $copyHeaderFields,
+            $this->Mail->DKIM_Add($headerLines, $subject, ''),
+            'DKIM header without copied header fields incorrect'
+        );
+    }
+
+    /**
+     * DKIM signing extra headers tests.
+     *
+     * @requires extension openssl
+     *
+     * @covers \PHPMailer\PHPMailer\PHPMailer::DKIM_Add
+     * @covers \PHPMailer\PHPMailer\PHPMailer::DKIM_Sign
+     * @covers \PHPMailer\PHPMailer\PHPMailer::DKIM_QP
+     */
+    public function testDKIMExtraHeaders()
+    {
+        $pk = openssl_pkey_new(
+            [
+                'private_key_bits' => 2048,
+                'private_key_type' => OPENSSL_KEYTYPE_RSA,
+            ]
+        );
+        openssl_pkey_export_to_file($pk, self::PRIVATE_KEY_FILE);
+        $this->Mail->DKIM_private = self::PRIVATE_KEY_FILE;
+
+        // Example from https://tools.ietf.org/html/rfc6376#section-3.5.
+        $from = 'from@example.com';
+        $to = 'to@example.com';
+        $date = 'date';
+        $subject = 'example';
+        $anyHeader = 'foo';
+        $unsubscribeUrl = '<https://www.example.com/unsubscribe/?newsletterId=anytoken&amp;actionToken=anyToken' .
+                            '&otherParam=otherValue&anotherParam=anotherVeryVeryVeryLongValue>';
+
+        $this->Mail->addCustomHeader('X-AnyHeader', $anyHeader);
+        $this->Mail->addCustomHeader('Baz', 'bar');
+        $this->Mail->addCustomHeader('List-Unsubscribe', $unsubscribeUrl);
+
+        $this->Mail->DKIM_extraHeaders = ['Baz', 'List-Unsubscribe'];
+
+        $headerLines = "From:$from\r\nTo:$to\r\nDate:$date\r\n";
+        $headerLines .= "X-AnyHeader:$anyHeader\r\nBaz:bar\r\n";
+        $headerLines .= 'List-Unsubscribe:' . $this->Mail->encodeHeader($unsubscribeUrl) . "\r\n";
+
+        $headerFields = 'h=From:To:Date:Baz:List-Unsubscribe:Subject';
+
+        $result = $this->Mail->DKIM_Add($headerLines, $subject, '');
+
+        self::assertStringContainsString($headerFields, $result, 'DKIM header with extra headers incorrect');
+    }
+
+    /**
+     * DKIM Signing tests.
+     *
+     * @requires extension openssl
+     *
+     * @covers \PHPMailer\PHPMailer\PHPMailer::DKIM_Add
+     * @covers \PHPMailer\PHPMailer\PHPMailer::DKIM_Sign
+     * @covers \PHPMailer\PHPMailer\PHPMailer::DKIM_QP
+     */
+    public function testDKIMSigningMail()
+    {
+        $this->Mail->Subject .= ': DKIM signing';
+        $this->Mail->Body = 'This message is DKIM signed.';
+        $this->buildBody();
+
+        // Make a new key pair.
+        // Note: 2048 bits is the recommended minimum key length - gmail won't accept less than 1024 bits.
+        $pk = openssl_pkey_new(
+            [
+                'private_key_bits' => 2048,
+                'private_key_type' => OPENSSL_KEYTYPE_RSA,
+            ]
+        );
+        openssl_pkey_export_to_file($pk, self::PRIVATE_KEY_FILE);
+
+        $this->Mail->DKIM_domain = 'example.com';
+        $this->Mail->DKIM_private = self::PRIVATE_KEY_FILE;
+        $this->Mail->DKIM_selector = 'phpmailer';
+        $this->Mail->DKIM_passphrase = ''; //key is not encrypted
+        self::assertTrue($this->Mail->send(), 'DKIM signed mail failed');
+
+        $this->Mail->isMail();
+        self::assertTrue($this->Mail->send(), 'DKIM signed mail via mail() failed');
+    }
+
+    /**
+     * Verify behaviour of the DKIM_Sign method when Open SSL is not available.
+     *
+     * @covers \PHPMailer\PHPMailer\PHPMailer::DKIM_Sign
+     */
+    public function testDKIMSignOpenSSLNotAvailable()
+    {
+        if (extension_loaded('openssl')) {
+            $this->markTestSkipped('Test requires OpenSSL *not* to be available');
+        }
+
+        $signature = $this->Mail->DKIM_Sign('foo');
+        self::assertSame('', $signature);
+    }
+
+    /**
+     * Verify behaviour of the DKIM_Sign method when Open SSL is not available and exceptions is enabled.
+     *
+     * @covers \PHPMailer\PHPMailer\PHPMailer::DKIM_Sign
+     */
+    public function testDKIMSignOpenSSLNotAvailableException()
+    {
+        if (extension_loaded('openssl')) {
+            $this->markTestSkipped('Test requires OpenSSL *not* to be available');
+        }
+
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('Extension missing: openssl');
+
+        $mail = new PHPMailer(true);
+        $mail->DKIM_Sign('foo');
+    }
+}
