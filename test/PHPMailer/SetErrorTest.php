058ded57b85cfb9dcbe136f97e2fe5399f8ca7aa
jrfnl <jrfnl@users.noreply.github.com>
2021-07-08 02:40:52 +0200

    SetErrorTest: add additional tests for the `PHPMailer::setError()` and `PHPMailer::isError()` methods
    So far, these methods were only tested in the most perfunctory manner.
    
    The additional tests this commit introduces, test all aspects of the methods as well as documents the current behaviour of the methods.
    
    Take note of the "text merging"/readability issues for the SMTP error messages. There may be room for improvement there.

--- test/PHPMailer/SetErrorTest.php
+++ test/PHPMailer/SetErrorTest.php
@@ -13,6 +13,7 @@
 
 namespace PHPMailer\Test\PHPMailer;
 
+use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\Test\TestCase;
 
 /**
@@ -37,5 +38,115 @@ final class SetErrorTest extends TestCase
             $this->Mail->ErrorInfo,
             'Error info not correctly set'
         );
+    }
+
+    /**
+     * Test simple, non-STMP, error registration, where only one of the two SMTP conditions is met.
+     */
+    public function testSetErrorNonSmtpWithMailerSmtp()
+    {
+        $this->Mail->Mailer = 'smtp';
+
+        $this->testSetErrorNonSMTP();
+    }
+
+    /**
+     * Test simple, non-STMP, error registration, where the other one of the two SMTP conditions is met.
+     */
+    public function testSetErrorNonSmtpWithSmtpInstanceSet()
+    {
+        $stub = $this->getMockBuilder(SMTP::class)->getMock();
+        $this->Mail->setSMTPInstance($stub);
+
+        $this->testSetErrorNonSMTP();
+    }
+
+    /**
+     * Test error registration with SMTP enabled and instantiated.
+     *
+     * @dataProvider dataSetErrorSmtp
+     *
+     * @param array  $mockReturn The value the `SMTP::getError()` method mock should return.
+     * @param string $expected   The error message which is expected to be registered.
+     */
+    public function testSetErrorSmtp($mockReturn, $expected)
+    {
+        $stub = $this->getMockBuilder(SMTP::class)->getMock();
+        $stub->method('getError')
+             ->willReturn($mockReturn);
+
+        $this->Mail->Mailer = 'smtp';
+        $this->Mail->setSMTPInstance($stub);
+
+        // "Abuse" the `PHPMailer::set()` method to register an error.
+        self::assertFalse($this->Mail->set('nonexistentproperty', 'value'));
+
+        self::assertTrue($this->Mail->isError(), 'Error count not incremented');
+        self::assertSame($expected, $this->Mail->ErrorInfo, 'Error info not correctly set');
+    }
+
+    /**
+     * Data provider.
+     *
+     * @return array
+     */
+    public function dataSetErrorSmtp()
+    {
+        return [
+            'No SMTP error' => [
+                'mockReturn' => [
+                    'error'        => '',
+                    'detail'       => '',
+                    'smtp_code'    => '',
+                    'smtp_code_ex' => '',
+                ],
+                'expected' => 'Cannot set or reset variable: nonexistentproperty',
+            ],
+            'SMTP error, no details' => [
+                'mockReturn' => [
+                    'error'        => 'Fake error',
+                    'detail'       => '',
+                    'smtp_code'    => '',
+                    'smtp_code_ex' => '',
+                ],
+                'expected' => 'Cannot set or reset variable: nonexistentpropertySMTP server error: Fake error',
+            ],
+            'SMTP error, full details' => [
+                'mockReturn' => [
+                    'error'        => 'Fake error',
+                    'detail'       => 'Fake detail',
+                    'smtp_code'    => 'Fake code',
+                    'smtp_code_ex' => 'Fake code ex',
+                ],
+                'expected' => 'Cannot set or reset variable: nonexistentpropertySMTP server error: '
+                    . 'Fake error Detail: Fake detail SMTP code: Fake code Additional SMTP info: Fake code ex',
+            ],
+        ];
+    }
+
+    /**
+     * Verify that only the last error registered via `setError()` is available.
+     */
+    public function testErrorInfoOnlyContainsLastError()
+    {
+        // "Abuse" the `PHPMailer::set()` method to register a few errors.
+        self::assertFalse($this->Mail->set('property1', 'value'));
+        self::assertFalse($this->Mail->set('property2', 'value'));
+        self::assertFalse($this->Mail->set('property3', 'value'));
+
+        self::assertTrue($this->Mail->isError(), 'Error count not incremented');
+        self::assertSame(
+            'Cannot set or reset variable: property3',
+            $this->Mail->ErrorInfo,
+            'Error info not correctly set'
+        );
+    }
+
+    /**
+     * Verify that when no errors have been registered, `isError()` returns false.
+     */
+    public function testIsErrorWithoutError()
+    {
+        self::assertFalse($this->Mail->isError(), 'Error count not 0');
     }
 }
