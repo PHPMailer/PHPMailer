05e70e859cc5872d10ad5a3f183226a89e586264
Michael Newton <miken32@gmail.com>
2021-10-11 11:45:39 -0400

    throw exception with details for connection failures

--- src/PHPMailer.php
+++ src/PHPMailer.php
@@ -2174,6 +2174,10 @@ class PHPMailer
         //As we've caught all exceptions, just report whatever the last one was
         if ($this->exceptions && null !== $lastexception) {
             throw $lastexception;
+        } elseif ($this->exceptions) {
+            // no exception was thrown, likely $this->smtp->connect() failed
+            $message = $this->getSmtpErrorMessage('connect_host');
+            throw new Exception($message);
         }
 
         return false;
