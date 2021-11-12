61d2d0251541f45bab2468f9346cadd5f6575c6b
Fonata <Fonata@users.noreply.github.com>
2021-11-12 13:59:28 +0100

    Improve spelling in DocBlock

--- src/SMTP.php
+++ src/SMTP.php
@@ -696,7 +696,7 @@ class SMTP
     /**
      * Send an SMTP DATA command.
      * Issues a data command and sends the msg_data to the server,
-     * finializing the mail transaction. $msg_data is the message
+     * finalizing the mail transaction. $msg_data is the message
      * that is to be send with the headers. Each header needs to be
      * on a single line followed by a <CRLF> with the message headers
      * and the message body being separated by an additional <CRLF>.
