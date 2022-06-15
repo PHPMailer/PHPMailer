3aaac73c6dc7c2d5aa6b5ba5fb964e8c8a6bad9e
Marcus Bointon <marcus@synchromedia.co.uk>
2022-06-15 22:05:22 +0200

    Improve docs on addEmbeddedImage

--- src/PHPMailer.php
+++ src/PHPMailer.php
@@ -3713,16 +3713,16 @@ class PHPMailer
      * These differ from 'regular' attachments in that they are intended to be
      * displayed inline with the message, not just attached for download.
      * This is used in HTML messages that embed the images
-     * the HTML refers to using the $cid value.
+     * the HTML refers to using the `$cid` value in `img` tags, for example `<img src="cid:mylogo">`.
      * Never use a user-supplied path to a file!
      *
      * @param string $path        Path to the attachment
      * @param string $cid         Content ID of the attachment; Use this to reference
      *                            the content when using an embedded image in HTML
-     * @param string $name        Overrides the attachment name
-     * @param string $encoding    File encoding (see $Encoding)
-     * @param string $type        File MIME type
-     * @param string $disposition Disposition to use
+     * @param string $name        Overrides the attachment filename
+     * @param string $encoding    File encoding (see $Encoding) defaults to `base64`
+     * @param string $type        File MIME type (by default mapped from the `$path` filename's extension)
+     * @param string $disposition Disposition to use: `inline` (default) or `attachment` (unlikely you want this – {@see `addAttachment()`} instead)
      *
      * @throws Exception
      *
