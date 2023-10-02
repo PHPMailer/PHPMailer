358be9a4673a21d8bc3c641e0dac291bd1989e09
Marcus Bointon <marcus@synchromedia.co.uk>
2023-10-02 11:21:51 +0200

    CS and early continue
    If it's null or empty, we know the next test will fail too, so continue straight away

--- src/PHPMailer.php
+++ src/PHPMailer.php
@@ -1571,8 +1571,9 @@ class PHPMailer
 
             //Validate From, Sender, and ConfirmReadingTo addresses
             foreach (['From', 'Sender', 'ConfirmReadingTo'] as $address_kind) {
-                if($this->{$address_kind} === null) {
+                if ($this->{$address_kind} === null) {
                     $this->{$address_kind} = '';
+                    continue;
                 }
                 $this->{$address_kind} = trim($this->{$address_kind});
                 if (empty($this->{$address_kind})) {
