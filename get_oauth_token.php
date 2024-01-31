5251d331249900a8551442391265a3ec20200c8f
Marcus Bointon <marcus@synchromedia.co.uk>
2024-01-31 23:05:36 +0100

    Escape refresh token

--- get_oauth_token.php
+++ get_oauth_token.php
@@ -178,5 +178,5 @@ if (!isset($_GET['code'])) {
     );
     //Use this to interact with an API on the users behalf
     //Use this to get a new access token if the old one expires
-    echo 'Refresh Token: ', $token->getRefreshToken();
+    echo 'Refresh Token: ', htmlspecialchars($token->getRefreshToken());
 }
