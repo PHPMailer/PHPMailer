cad92ca7bd8e740b007f32f9409639f76d739e48
ProjectSoft <projectsoft2009@yandex.ru>
2024-07-20 16:48:39 +0400

    Full translation into Russian

--- language/phpmailer.lang-ru.php
+++ language/phpmailer.lang-ru.php
@@ -5,24 +5,32 @@
  * @package PHPMailer
  * @author Alexey Chumakov <alex@chumakov.ru>
  * @author Foster Snowhill <i18n@forstwoof.ru>
+ * @author ProjectSoft <projectsoft2009@yandex.ru>
  */
 
-$PHPMAILER_LANG['authenticate']         = 'Ошибка SMTP: ошибка авторизации.';
+$PHPMAILER_LANG['authenticate']         = 'Ошибка SMTP: не удалось пройти аутентификацию.';
+$PHPMAILER_LANG['buggy_php']            = 'В вашей версии PHP есть ошибка, которая может привести к повреждению сообщений. Чтобы исправить, переключитесь на отправку по SMTP, отключите опцию mail.add_x_header в ваш php.ini, переключитесь на MacOS или Linux или обновите PHP до версии 7.0.17+ или 7.1.3+.';
 $PHPMAILER_LANG['connect_host']         = 'Ошибка SMTP: не удается подключиться к SMTP-серверу.';
 $PHPMAILER_LANG['data_not_accepted']    = 'Ошибка SMTP: данные не приняты.';
+$PHPMAILER_LANG['empty_message']        = 'Пустое сообщение';
 $PHPMAILER_LANG['encoding']             = 'Неизвестная кодировка: ';
 $PHPMAILER_LANG['execute']              = 'Невозможно выполнить команду: ';
+$PHPMAILER_LANG['extension_missing']    = 'Расширение отсутствует: ';
 $PHPMAILER_LANG['file_access']          = 'Нет доступа к файлу: ';
 $PHPMAILER_LANG['file_open']            = 'Файловая ошибка: не удаётся открыть файл: ';
 $PHPMAILER_LANG['from_failed']          = 'Неверный адрес отправителя: ';
 $PHPMAILER_LANG['instantiate']          = 'Невозможно запустить функцию mail().';
-$PHPMAILER_LANG['provide_address']      = 'Пожалуйста, введите хотя бы один email-адрес получателя.';
-$PHPMAILER_LANG['mailer_not_supported'] = ' — почтовый сервер не поддерживается.';
-$PHPMAILER_LANG['recipients_failed']    = 'Ошибка SMTP: не удалась отправка таким адресатам: ';
-$PHPMAILER_LANG['empty_message']        = 'Пустое сообщение';
 $PHPMAILER_LANG['invalid_address']      = 'Не отправлено из-за неправильного формата email-адреса: ';
+$PHPMAILER_LANG['invalid_header']       = 'Неверное имя или значение заголовка';
+$PHPMAILER_LANG['invalid_hostentry']    = 'Неверная запись хоста: ';
+$PHPMAILER_LANG['invalid_host']         = 'Неверный хост: ';
+$PHPMAILER_LANG['mailer_not_supported'] = ' — почтовый сервер не поддерживается.';
+$PHPMAILER_LANG['provide_address']      = 'Вы должны указать хотя бы один адрес электронной почты получателя.';
+$PHPMAILER_LANG['recipients_failed']    = 'Ошибка SMTP: Ошибка следующих получателей: ';
 $PHPMAILER_LANG['signing']              = 'Ошибка подписи: ';
-$PHPMAILER_LANG['smtp_connect_failed']  = 'Ошибка соединения с SMTP-сервером';
+$PHPMAILER_LANG['smtp_code']            = 'Код SMTP: ';
+$PHPMAILER_LANG['smtp_code_ex']         = 'Дополнительная информация SMTP: ';
+$PHPMAILER_LANG['smtp_connect_failed']  = 'Ошибка соединения с SMTP-сервером.';
+$PHPMAILER_LANG['smtp_detail']          = 'Детали: ';
 $PHPMAILER_LANG['smtp_error']           = 'Ошибка SMTP-сервера: ';
 $PHPMAILER_LANG['variable_set']         = 'Невозможно установить или сбросить переменную: ';
-$PHPMAILER_LANG['extension_missing']    = 'Расширение отсутствует: ';
