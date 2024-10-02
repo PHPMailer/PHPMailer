df4648947987929db020503f6b68376b4e2ae5bd
Marcus Bointon <marcus@synchromedia.co.uk>
2024-10-02 23:26:04 +0200

    Add Kurdish Sorani translation

---
+++ language/phpmailer.lang-ku.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Kurdish (Sorani) PHPMailer language file: refer to English translation for definitive list
+ * @package PHPMailer
+ * @author Halo Salman <halo@home4t.com>
+ */
+
+$PHPMAILER_LANG['authenticate']         = 'هەڵەی SMTP : نەتوانرا کۆدەکە پشتڕاست بکرێتەوە ';
+$PHPMAILER_LANG['connect_host']         = 'هەڵەی SMTP: نەتوانرا پەیوەندی بە سێرڤەرەوە بکات SMTP.';
+$PHPMAILER_LANG['data_not_accepted']    = 'هەڵەی SMTP: ئەو زانیاریانە قبوڵ نەکرا.';
+$PHPMAILER_LANG['empty_message']        = 'پەیامەکە بەتاڵە';
+$PHPMAILER_LANG['encoding']             = 'کۆدکردنی نەزانراو : ';
+$PHPMAILER_LANG['execute']              = 'ناتوانرێت جێبەجێ بکرێت: ';
+$PHPMAILER_LANG['file_access']          = 'ناتوانرێت دەستت بگات بە فایلەکە: ';
+$PHPMAILER_LANG['file_open']            = 'هەڵەی پەڕگە(فایل): ناتوانرێت بکرێتەوە: ';
+$PHPMAILER_LANG['from_failed']          = 'هەڵە لە ئاستی ناونیشانی نێرەر: ';
+$PHPMAILER_LANG['instantiate']          = 'ناتوانرێت خزمەتگوزاری پۆستە پێشکەش بکرێت.';
+$PHPMAILER_LANG['invalid_address']      = 'نەتوانرا بنێردرێت ، چونکە ناونیشانی ئیمەیڵەکە نادروستە: ';
+$PHPMAILER_LANG['mailer_not_supported'] = ' مەیلەر پشتگیری ناکات';
+$PHPMAILER_LANG['provide_address']      = 'دەبێت ناونیشانی ئیمەیڵی لانیکەم یەک وەرگر دابین بکرێت.';
+$PHPMAILER_LANG['recipients_failed']    = ' هەڵەی SMTP: ئەم هەڵانەی خوارەوەشکستی هێنا لە ناردن بۆ هەردووکیان: ';
+$PHPMAILER_LANG['signing']              = 'هەڵەی واژۆ: ';
+$PHPMAILER_LANG['smtp_connect_failed']  = 'SMTP Connect()پەیوەندی شکستی هێنا .';
+$PHPMAILER_LANG['smtp_error']           = 'هەڵەی ئاستی سێرڤەری SMTP: ';
+$PHPMAILER_LANG['variable_set']         = 'ناتوانرێت بیگۆڕیت یان دوبارە بینێریتەوە: ';
+$PHPMAILER_LANG['extension_missing']    = 'درێژکراوە نەماوە: ';
