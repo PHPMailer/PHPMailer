7ff78e72e8224f9c978ca6f0ac926999e5a5f799
Synchro <marcus@synchromedia.co.uk>
2025-02-02 20:34:43 +0000

    Deploying to gh-pages from @ PHPMailer/PHPMailer@ccce6f6ce9e4836d86467e37a41a71764d05044a 🚀

--- js/template.js
+++ js/template.js
@@ -15,3 +15,20 @@
         observer.observe(el);
     })
 })();
+function openSvg(svg) {
+    // convert to a valid XML source
+    const as_text = new XMLSerializer().serializeToString(svg);
+    // store in a Blob
+    const blob = new Blob([as_text], { type: "image/svg+xml" });
+    // create an URI pointing to that blob
+    const url = URL.createObjectURL(blob);
+    const win = open(url);
+    // so the Garbage Collector can collect the blob
+    win.onload = (evt) => URL.revokeObjectURL(url);
+};
+
+
+var svgs = document.querySelectorAll(".phpdocumentor-uml-diagram svg");
+for( var i=0,il = svgs.length; i< il; i ++ ) {
+    svgs[i].onclick = (evt) => openSvg(evt.target);
+}
