bd21829c81bf5bb33f27327d6eb4ad5593521ae0
Bruce Bannon <github.ful9l@simplelogin.com>
2025-08-26 05:13:44 -0600

    well shoot! missed some ">"

--- language/phpmailer.lang-yo.php
+++ language/phpmailer.lang-yo.php
@@ -6,15 +6,15 @@
  * @package PHPMailer
  */
 
-$PHPMAILER_LANG['authenticate']         => 'Aṣiṣe SMTP: Ko le jẹrisi';
-$PHPMAILER_LANG['connect_host']         => 'Aṣiṣe SMTP: Ko le sopọ si agbalejo SMTP:';
-$PHPMAILER_LANG['data_not_accepted']    => 'Aṣiṣe SMTP: A ko gba data laaye';
-$PHPMAILER_LANG['encoding']             => 'Aiyipada koodu:';
-$PHPMAILER_LANG['execute']              => 'Ko le ṣe:';
-$PHPMAILER_LANG['file_access']          => 'Ko le wọle si faili:';
-$PHPMAILER_LANG['file_open']            => 'Aṣiṣe Faili Ko le ṣi faili:';
-$PHPMAILER_LANG['from_failed']          => 'Atẹle Lati Adirẹsi naa kuna:';
-$PHPMAILER_LANG['instantiate']          => 'Ko le ṣe atunṣe iṣẹ meeli';
-$PHPMAILER_LANG['mailer_not_supported'] => 'ifiweranse ko ni atilẹyin';
-$PHPMAILER_LANG['provide_address']      => 'O gbọdọ pese o kere ju adirẹsi imeeli olugba kan.';
-$PHPMAILER_LANG['recipients_failed']    => 'Aṣiṣe SMTP: Awọn olugba wọnyi kuna:';
+$PHPMAILER_LANG['authenticate']         = 'Aṣiṣe SMTP: Ko le jẹrisi';
+$PHPMAILER_LANG['connect_host']         = 'Aṣiṣe SMTP: Ko le sopọ si agbalejo SMTP:';
+$PHPMAILER_LANG['data_not_accepted']    = 'Aṣiṣe SMTP: A ko gba data laaye';
+$PHPMAILER_LANG['encoding']             = 'Aiyipada koodu:';
+$PHPMAILER_LANG['execute']              = 'Ko le ṣe:';
+$PHPMAILER_LANG['file_access']          = 'Ko le wọle si faili:';
+$PHPMAILER_LANG['file_open']            = 'Aṣiṣe Faili Ko le ṣi faili:';
+$PHPMAILER_LANG['from_failed']          = 'Atẹle Lati Adirẹsi naa kuna:';
+$PHPMAILER_LANG['instantiate']          = 'Ko le ṣe atunṣe iṣẹ meeli';
+$PHPMAILER_LANG['mailer_not_supported'] = 'ifiweranse ko ni atilẹyin';
+$PHPMAILER_LANG['provide_address']      = 'O gbọdọ pese o kere ju adirẹsi imeeli olugba kan.';
+$PHPMAILER_LANG['recipients_failed']    = 'Aṣiṣe SMTP: Awọn olugba wọnyi kuna:';
